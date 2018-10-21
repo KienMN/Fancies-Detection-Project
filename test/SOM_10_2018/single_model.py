0e51235ca9c887749dde1588b9af8a42abe080c3
Kien Mai Ngoc <kienmn97@gmail.com>
2018-10-21 18:50:32 +0700

    Test single model for new data

---
+++ test/SOM_10_2018/single_model.py
@@ -0,0 +1,56 @@
+# LVQ with Neighborhood Model
+
+# Adding path to libraries
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+# Importing the libraries
+import numpy as np
+import pandas as pd
+
+# Importing the dataset
+filepath = os.path.join(os.path.dirname(__file__), 'data/filtered_RUBY-2X.csv')
+dataset = pd.read_csv(filepath)
+X = dataset.iloc[:, 2: -1].values
+y = dataset.iloc[:, -1].values.astype(np.int8)
+
+# Spliting the dataset into the Training set and the Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0, shuffle = False)
+
+# Feature scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler(feature_range = (-1, 1))
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+# Label encoder
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+y_train = encoder.fit_transform(y_train)
+
+# Training the LVQ
+from detection.competitive_learning_network import AdaptiveLVQ
+lvq = AdaptiveLVQ(n_rows = 12, n_cols = 12,
+                  learning_rate = 0.5, decay_rate = 1,
+                  sigma = 2, sigma_decay_rate = 1,
+                  # weights_normalization = "length",
+                  bias = False, weights_init = 'pca',
+                  neighborhood='gaussian', label_weight = 'inverse_distance')
+lvq.fit(X_train, y_train, first_num_iteration = 15000, first_epoch_size = 400, second_num_iteration = 15000, second_epoch_size = 400)
+
+# Predict the result
+y_pred, confidence_score = lvq.predict(X_test, confidence = 1)
+y_pred = encoder.inverse_transform(y_pred)
+
+# Making confusion matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Printing the confusion matrix
+print(cm)
+true_result = 0
+for i in range (len(cm)):
+  true_result += cm[i][i]
+print(true_result / np.sum(cm))
