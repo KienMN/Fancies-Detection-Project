cfc490f6c6475db88aa2487d0d84cee0ec3854ff
Kien Mai Ngoc <kienmn97@gmail.com>
2018-10-23 18:16:24 +0700

    Editing script for combined features

--- test/SOM_10_2018/test_script_3.sh
+++ test/SOM_10_2018/test_script_3.sh
@@ -1,4 +1,4 @@
-bsub "python combined_model_features.py -d 1X -m 234-123,234-234,234-345 -gc 1,2,3-2,3,4-3,4,5 > result/combined_234_123-234-345.txt 2>&1"
-bsub "python combined_model_features.py -d 2X -m 134-123,134-234,134-345 -gc 1,2,3-2,3,4-3,4,5 > result/combined_134_123-234-345.txt 2>&1"
-bsub "python combined_model_features.py -d 3X -m 124-123,124-234,124-345 -gc 1,2,3-2,3,4-3,4,5 > result/combined_124_123-234-345.txt 2>&1"
-bsub "python combined_model_features.py -d 4X -m 123-123,123-234,123-345 -gc 1,2,3-2,3,4-3,4,5 > result/combined_123_123-234-345.txt 2>&1"
+bsub "python combined_model_features.py -d 1X -m 234-123-gaussian,234-234-gaussian,234-345-gaussian -gc 1,2,3-2,3,4-3,4,5 > result/combined_234_123-234-345.txt 2>&1"
+bsub "python combined_model_features.py -d 2X -m 134-123-gaussian,134-234-gaussian,134-345-gaussian -gc 1,2,3-2,3,4-3,4,5 > result/combined_134_123-234-345.txt 2>&1"
+bsub "python combined_model_features.py -d 3X -m 124-123-gaussian,124-234-gaussian,124-345-gaussian -gc 1,2,3-2,3,4-3,4,5 > result/combined_124_123-234-345.txt 2>&1"
+bsub "python combined_model_features.py -d 4X -m 123-123-gaussian,123-234-gaussian,123-345-gaussian -gc 1,2,3-2,3,4-3,4,5 > result/combined_123_123-234-345.txt 2>&1"
