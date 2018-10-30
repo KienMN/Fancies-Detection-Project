3cf9e443245fef196fd03fead171494d7fb88b0d
Kien Mai Ngoc <kienmn97@gmail.com>
2018-10-30 14:07:45 +0700

    new script for combining 4 features

--- test/SOM_10_2018/test_script_2.sh
+++ test/SOM_10_2018/test_script_2.sh
@@ -1,11 +1,14 @@
-bsub "python model-features.py -tr 1X,2X,3X -te 4X -m 123-13-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 1,3 > result/123_13_gaussian.txt 2>&1"
-bsub "python model-features.py -tr 1X,2X,3X -te 4X -m 123-245-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 2,4,5 > result/123_245_gaussian.txt 2>&1"
+bsub "python model-features.py -tr 1X,2X,3X -te 4X -m 123-1234-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 1,2,3,4 > result/123_1234_gaussian.txt 2>&1"
+bsub "python model-features.py -tr 1X,2X,3X -te 4X -m 123-1235-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 1,2,3,5 > result/123_1235_gaussian.txt 2>&1"
+bsub "python model-features.py -tr 1X,2X,3X -te 4X -m 123-1245-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 1,2,4,5 > result/123_1245_gaussian.txt 2>&1"
+bsub "python model-features.py -tr 1X,2X,3X -te 4X -m 123-1345-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 1,3,4,5 > result/123_1345_gaussian.txt 2>&1"
+bsub "python model-features.py -tr 1X,2X,3X -te 4X -m 123-2345-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 2,3,4,5 > result/123_2345_gaussian.txt 2>&1"
 
-bsub "python model-features.py -tr 1X,2X,4X -te 3X -m 124-13-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 1,3 > result/124_13_gaussian.txt 2>&1"
-bsub "python model-features.py -tr 1X,2X,4X -te 3X -m 124-245-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 2,4,5 > result/124_245_gaussian.txt 2>&1"
+# bsub "python model-features.py -tr 1X,2X,4X -te 3X -m 124-13-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 1,3 > result/124_13_gaussian.txt 2>&1"
+# bsub "python model-features.py -tr 1X,2X,4X -te 3X -m 124-245-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 2,4,5 > result/124_245_gaussian.txt 2>&1"
 
-bsub "python model-features.py -tr 1X,3X,4X -te 2X -m 134-13-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 1,3 > result/134_13_gaussian.txt 2>&1"
-bsub "python model-features.py -tr 1X,3X,4X -te 2X -m 134-245-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 2,4,5 > result/134_245_gaussian.txt 2>&1"
+# bsub "python model-features.py -tr 1X,3X,4X -te 2X -m 134-13-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 1,3 > result/134_13_gaussian.txt 2>&1"
+# bsub "python model-features.py -tr 1X,3X,4X -te 2X -m 134-245-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 2,4,5 > result/134_245_gaussian.txt 2>&1"
 
-bsub "python model-features.py -tr 2X,3X,4X -te 1X -m 234-13-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 1,3 > result/234_13_gaussian.txt 2>&1"
-bsub "python model-features.py -tr 2X,3X,4X -te 1X -m 234-245-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 2,4,5 > result/234_245_gaussian.txt 2>&1"
+# bsub "python model-features.py -tr 2X,3X,4X -te 1X -m 234-13-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 1,3 > result/234_13_gaussian.txt 2>&1"
+# bsub "python model-features.py -tr 2X,3X,4X -te 1X -m 234-245-gaussian -s 30 -fi 30000 -si 30000 -lr 0.75 -sig 3 -n gaussian -c 2,4,5 > result/234_245_gaussian.txt 2>&1"
