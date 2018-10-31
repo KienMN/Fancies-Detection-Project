a6cf5f4d68c7f89b133b440c363d40e12f2f3b55
Kien Mai Ngoc <kienmn97@gmail.com>
2018-10-31 16:11:49 +0700

    Script for combining 4 features

--- test/SOM_10_2018/combined_model_features.py
+++ test/SOM_10_2018/combined_model_features.py
@@ -45,6 +45,7 @@ X_test = sc.transform(X_test)
 y_pred, confidence_score = lvq.predict(X_test, confidence = 1, crit = 'winner_neuron')
 y_pred = encoder.inverse_transform(y_pred)
 y_pred = y_pred.reshape((-1, 1))
+confidence_score = confidence_score.reshape((-1, 1))
 
 for i in range (1, len(models)):
   model_name = models[i]
@@ -63,7 +64,7 @@ for i in range (1, len(models)):
   y_pred_tmp, confidence_score_tmp = lvq.predict(X_test, confidence = 1, crit = 'winner_neuron')
   y_pred_tmp = encoder.inverse_transform(y_pred_tmp)
   y_pred = np.append(y_pred, y_pred_tmp.reshape((-1, 1)), axis = 1)
-  confidence_score = confidence_score + confidence_score_tmp
+  confidence_score = np.append(confidence_score, confidence_score_tmp.reshape((-1, 1)), axis = 1)
 
 # Combining
 print(confidence_score.shape)
@@ -74,9 +75,7 @@ y_pred_combined = np.array([]).astype(np.int8)
 n_samples = len(y)
 for i in range (n_samples):
   j = np.argmax(confidence_score[i])
-  y_pred_combined = np.append(y_pred_combined, j)
-
-y_pred_combined = encoder.inverse_transform(y_pred_combined)
+  y_pred_combined = np.append(y_pred_combined, y_pred[i, j])
 
 # Making confusion matrix
 from sklearn.metrics import confusion_matrix
--- test/SOM_10_2018/test_script_3.sh
+++ test/SOM_10_2018/test_script_3.sh
@@ -1,9 +1,12 @@
-bsub "python combined_model_features.py -d 1X -m 234-13-gaussian,234-245-gaussian -gc 1,3-2,4,5 > result/combined_sum_234_gaussian_13-245.txt 2>&1"
-bsub "python combined_model_features.py -d 2X -m 134-13-gaussian,134-245-gaussian -gc 1,3-2,4,5 > result/combined_sum_134_gaussian_13-245.txt 2>&1"
-bsub "python combined_model_features.py -d 3X -m 124-13-gaussian,124-245-gaussian -gc 1,3-2,4,5 > result/combined_sum_124_gaussian_13-245.txt 2>&1"
-bsub "python combined_model_features.py -d 4X -m 123-13-gaussian,123-245-gaussian -gc 1,3-2,4,5 > result/combined_sum_123_gaussian_13-245.txt 2>&1"
+# bsub "python combined_model_features.py -d 1X -m 234-13-gaussian,234-245-gaussian -gc 1,3-2,4,5 > result/combined_sum_234_gaussian_13-245.txt 2>&1"
+# bsub "python combined_model_features.py -d 2X -m 134-13-gaussian,134-245-gaussian -gc 1,3-2,4,5 > result/combined_sum_134_gaussian_13-245.txt 2>&1"
+# bsub "python combined_model_features.py -d 3X -m 124-13-gaussian,124-245-gaussian -gc 1,3-2,4,5 > result/combined_sum_124_gaussian_13-245.txt 2>&1"
+# bsub "python combined_model_features.py -d 4X -m 123-13-gaussian,123-245-gaussian -gc 1,3-2,4,5 > result/combined_sum_123_gaussian_13-245.txt 2>&1"
 
 # bsub "python combined_model_features.py -d 1X -m 234-123-gaussian,234-45-gaussian -gc 1,2,3-4,5 > result/combined_sum_234_gaussian_123-45.txt 2>&1"
 # bsub "python combined_model_features.py -d 2X -m 134-123-gaussian,134-45-gaussian -gc 1,2,3-4,5 > result/combined_sum_134_gaussian_123-45.txt 2>&1"
 # bsub "python combined_model_features.py -d 3X -m 124-123-gaussian,124-45-gaussian -gc 1,2,3-4,5 > result/combined_sum_124_gaussian_123-45.txt 2>&1"
 # bsub "python combined_model_features.py -d 4X -m 123-123-gaussian,123-45-gaussian -gc 1,2,3-4,5 > result/combined_sum_123_gaussian_123-45.txt 2>&1"
+
+bsub "python combined_model_features.py -d 4X -m 123-1234-gaussian,123-1235-gaussian,123-1245-gaussian,123-1345-gaussian,123-2345-gaussian -gc 1,2,3,4-1,2,3,5-1,2,4,5-1,3,4,5-2,3,4,5 > result/combined_sum_123_gaussian_1234-1235-1245-1345-2345.txt 2>&1"
+bsub "python combined_model_features.py -d 1X -m 234-1234-gaussian,234-1235-gaussian,234-1245-gaussian,234-1345-gaussian,234-2345-gaussian -gc 1,2,3,4-1,2,3,5-1,2,4,5-1,3,4,5-2,3,4,5 > result/combined_sum_234_gaussian_1234-1235-1245-1345-2345.txt 2>&1"
